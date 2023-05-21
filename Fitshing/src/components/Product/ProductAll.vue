4f6731eec03e298367ee35eed493eef3df8d439d
Jeerawat Maiwai <102474034+JeerawatM@users.noreply.github.com>
2023-05-21 16:12:50 +0700

    fixproduct

--- Fitshing/src/components/Product/ProductAll.vue
+++ Fitshing/src/components/Product/ProductAll.vue
@@ -1,12 +1,11 @@
 <template>
-  <section class="hero" style="">
-    <div class="hero-body container">
-      <div class="p-2">
+  <section class="hero container" style="">
+    <div class="hero-body ">
+      <div class="">
         <div class="columns">
           <div class="field has-addons column is-offset-8">
             <div style="display: flex" class="">
               <div class="control has-icons-left">
-                
                 <div class="select">
                   <select v-model="sort1">
                     <option value="">All</option>
@@ -27,50 +26,75 @@
             <button class="button ml-4 is-danger is-light" @click="clearCart()">
               Clear Cart
             </button>
-            <div style="display: flex; justify-content: space-between; font-size: 1.25rem; " >
+            <div
+              style="
+                display: flex;
+                justify-content: space-between;
+                font-size: 1.25rem;
+              "
+            >
               <router-link to="/cart">
-              <button 
-                v-if="cart.length !=0"
-                class="button is-warning ml-4"
-                style="width: 100%"
-                >Cart ({{cart.length}})</button>
-                </router-link>
+                <button
+                  v-if="cart.length != 0"
+                  class="button is-warning ml-4"
+                  style="width: 100%"
+                >
+                  Cart ({{ cart.length }})
+                </button>
+              </router-link>
             </div>
           </div>
         </div>
-        <div class="columns is-mobile">
+        <div class="is-mobile">
             <div class="columns is-multiline">
               <!-- แต่ละproduct -->
-              <div class="column is-3 has-text-centered cardd" v-for="(item, index) in filteredProduct" key="item.ex_id">
-                <div class="card has-text-centered" style="max-width:300px;height:350px;">
+              <div
+                class="column is-3 cardd"
+                v-for="(item, index) in filteredProduct"
+                key="item.ex_id"
+              >
+                <div
+                  class="card has-text-centered"
+                  style="max-width: 300px; height: 370px"
+                >
                   <div class="column">
-                  <figure class="image" >
-                    <img :src="imagePath(item.file_path)" style=" width:300px; height:200px;"  alt="Placeholder image" />
-                  </figure>
+                    <figure class="image">
+                      <img
+                        :src="imagePath(item.file_path)"
+                        style="object-fit: contain; width: 300px; height: 200px"
+                        alt="Placeholder image"
+                      />
+                    </figure>
                   </div>
-                  <p class="title is-6 m-3">{{item.ex_name}}</p>
+                  <p class="title is-6 m-3">{{ item.ex_name }}</p>
                   <p class="subtitle is-6 m-3">฿ {{ item.ex_price }}</p>
                   <div class="columns">
                     <div class="column is-6 has-text-centered">
-                      <router-link :to='`/product/preview/${item.ex_id}`' >
-                      <button class="button b-detail is-success">detail</button>
+                      <router-link :to="`/product/preview/${item.ex_id}`">
+                        <button class="button b-detail is-success">
+                          detail
+                        </button>
                       </router-link>
                     </div>
                     <div class="column is-6 has-text-centered">
-                      <button v-if="check(item)"
+                      <button
+                        v-if="check(item)"
                         class="button b-addcart"
                         disabled
                       >
-                      In Cart
+                        In Cart
                       </button>
-                      <button v-else-if="item.amount <= 0"
+                      <button
+                        v-else-if="item.amount <= 0"
                         class="button b-addcart"
                         disabled
                       >
                         Sold out
                       </button>
-                      <button v-else
-                      class="button b-addcart is-warning"  style="width:100px;"
+                      <button
+                        v-else
+                        class="button b-addcart is-warning"
+                        style="width: 100px"
                         @click="AddCart(item)"
                       >
                         Add
@@ -79,18 +103,16 @@
                   </div>
                 </div>
               </div>
-        
+
           </div>
-          </div>
-            
-          </div>
-          </div>
+        </div>
+      </div>
+    </div>
   </section>
 </template>
 
 
 <script setup>
-
 </script>
 
 <script>
@@ -101,23 +123,21 @@ export default {
     return {
       sort1: "",
       product: [],
-      cart:[],
-      filteredProduct: []
-      
-      
+      cart: [],
+      filteredProduct: [],
     };
   },
-  
+
   mounted() {
     this.cart = JSON.parse(localStorage.cart);
     this.getProduct();
   },
   methods: {
-    check(product){
-        if(this.cart.includes(product)){
-          return true
-        }
-      },
+    check(product) {
+      if (this.cart.includes(product)) {
+        return true;
+      }
+    },
     getProduct() {
       axios
         .get("/product")
@@ -146,34 +166,32 @@ export default {
       localStorage.setItem("cart", JSON.stringify(this.cart));
     },
     AddCart(product) {
-      if (this.cart.includes(product,-1)) {
-        alert("คุณมีสินค้านี้ในตระก้าแล้ว")
+      if (this.cart.includes(product, -1)) {
+        alert("คุณมีสินค้านี้ในตระก้าแล้ว");
       } else {
         this.cart.push(product);
         product.quantity = 1;
-
       }
       localStorage.setItem("cart", JSON.stringify(this.cart));
     },
     removeFromCart(product) {
-      this.cart.splice(this.cart.indexOf(product), 1)
-    localStorage.setItem("cart", JSON.stringify(this.cart));
+      this.cart.splice(this.cart.indexOf(product), 1);
+      localStorage.setItem("cart", JSON.stringify(this.cart));
     },
   },
   computed: {
-      sumary() {
-        return this.cart[0]==this.cart[1]
-      },
-      filteredProduct() {
-
-      if (this.sort1 === '') {
+    sumary() {
+      return this.cart[0] == this.cart[1];
+    },
+    filteredProduct() {
+      if (this.sort1 === "") {
         // Filter by item category
         return this.product;
       }
       // Return all items if no filter option is selected
-      return this.product.filter(item => item.type1 === this.sort1);
+      return this.product.filter((item) => item.type1 === this.sort1);
     },
-    },
+  },
 };
 </script>
 
@@ -182,5 +200,4 @@ import CardProductVue from "../CardProduct.vue";
 </script>
 
 <style>
-
 </style>
